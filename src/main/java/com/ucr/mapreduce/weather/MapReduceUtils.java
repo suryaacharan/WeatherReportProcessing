c282a648ee8298ad4a5024a446db98c37e36b57d
Abhijit Kulkarni <akulk022@ucr.edu>
2022-11-24 19:21:10 -0800

    Months logic in utils

--- src/main/java/com/ucr/mapreduce/weather/MapReduceUtils.java
+++ src/main/java/com/ucr/mapreduce/weather/MapReduceUtils.java
@@ -54,4 +54,50 @@ public class MapReduceUtils {
 		}
 		return i;
 	}
+	
+	public static String getMonthNameFromNum(String monthNum) {
+		String monthName = "";
+		switch (monthNum) {
+		case "01":
+			monthName="Jan";
+			break;
+		case "02":
+			monthName="Feb";
+			break;
+		case "03":
+			monthName="Mar";
+			break;	
+		case "04":
+			monthName="Apr";
+			break;
+		case "05":
+			monthName="May";
+			break;
+		case "06":
+			monthName="Jun";
+			break;
+		case "07":
+			monthName="Jul";
+			break;
+		case "08":
+			monthName="Aug";
+			break;
+		case "09":
+			monthName="Sep";
+			break;
+		case "10":
+			monthName="Oct";
+			break;
+		case "11":
+			monthName="Nov";
+			break;
+		case "12":
+			monthName="Dec";
+			break;
+
+		default:
+			break;
+		}
+		return monthName;
+	}
 }
